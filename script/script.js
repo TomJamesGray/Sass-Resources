678f6bd3522b8c68f64a3c2aac9019ac82838bc0
Tom Gray <tomg77@yahoo.co.uk>
2015-02-17 18:49:33 +0000

    Start

--- script/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("uploadBtn").addEventListener('change',function(){
-	// Remove fakepath in chrome
-	var path = this.value;
- 	var filename = path.replace(/^.*\\/, "");
-	document.getElementById("uploadFileOutput").value = filename;
-}, false);
-
-// Slide on pages
-document.getElementById()
